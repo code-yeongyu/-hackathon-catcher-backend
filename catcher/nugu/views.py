edbb96a0e1a672980af908ae9336fb13ba90b0ae
YeonGyu <public.kim.yeon.gyu@gmail.com>
2019-12-22 06:33:12 +0900

    Add nugu support: Beta version

--- catcher/nugu/views.py
+++ catcher/nugu/views.py
@@ -1,6 +1,6 @@
 import json
 from rest_framework import status
-from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 
