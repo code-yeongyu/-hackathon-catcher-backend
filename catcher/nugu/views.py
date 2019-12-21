053c5e485add6913a40aeba7e92cdf62beed9966
YeonGyu <public.kim.yeon.gyu@gmail.com>
2019-12-22 06:23:14 +0900

    Add nugu support: Alpha version

--- catcher/nugu/views.py
+++ catcher/nugu/views.py
@@ -4,8 +4,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 
-NUGU_JSON = '{"response":{"version":"2.0","resultCode":"OK","output":{},"directives":[{"type":"AudioPlayer.Play","audioItem":{"stream":{"url":"","offsetInMilliseconds":0,"progressReport":{},"token":"","expectedPreviousToken":""},"metadata":{}}}]}}'
-NUGU_OBJ = json.loads(NUGU_JSON)
+NUGU_JSON_FIRST = '{"response":{"version":"2.0","resultCode":"OK","output":{'
+NUGU_JSON_SECOND = '},"directives":[{"type":"AudioPlayer.Play","audioItem":{"stream":{"url":"","offsetInMilliseconds":0,"progressReport":{},"token":"","expectedPreviousToken":""},"metadata":{}}}]}}'
+#NUGU_OBJ = json.loads(NUGU_JSON)
 
 
 @api_view(['POST'])
@@ -18,8 +19,6 @@ def location(request):
         response_str = role + "는 서울시 관악구 봉천동 4-1 에 있어!"
     else:
         response_str = role + "는 아직 등록 되어있지 않아."
-    NUGU_OBJ['response']['output']['str'] = response_str
-    NUGU_FORMAT = json.dumps(NUGU_OBJ)
-    import pdb
-    pdb.set_trace()
-    return Response(json.dumps(NUGU_OBJ), status=status.HTTP_200_OK)
+    a = f'"str":"{response_str}"'
+    return Response(NUGU_JSON_FIRST + a + NUGU_JSON_SECOND,
+                    status=status.HTTP_200_OK)
