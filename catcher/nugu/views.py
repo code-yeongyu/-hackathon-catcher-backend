84d43163a624a1d1cf26a005761c6a1ac1fc71e6
YeonGyu <public.kim.yeon.gyu@gmail.com>
2019-12-22 06:31:48 +0900

    Add nugu support: Beta version

--- catcher/nugu/views.py
+++ catcher/nugu/views.py
@@ -1,12 +1,11 @@
 import json
 from rest_framework import status
-from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 
-NUGU_JSON_FIRST = '{"response":{"version":"2.0","resultCode":"OK","output":{'
-NUGU_JSON_SECOND = '},"directives":[{"type":"AudioPlayer.Play","audioItem":{"stream":{"url":"","offsetInMilliseconds":0,"progressReport":{},"token":"","expectedPreviousToken":""},"metadata":{}}}]}}'
-#NUGU_OBJ = json.loads(NUGU_JSON)
+NUGU_JSON = '{"response":{"version":"2.0","resultCode":"OK","output":{},"directives":[{"type":"AudioPlayer.Play","audioItem":{"stream":{"url":"","offsetInMilliseconds":0,"progressReport":{},"token":"","expectedPreviousToken":""},"metadata":{}}}]}}'
+NUGU_OBJ = json.loads(NUGU_JSON)
 
 
 @api_view(['POST'])
@@ -14,14 +13,10 @@ NUGU_JSON_SECOND = '},"directives":[{"type":"AudioPlayer.Play","audioItem":{"str
 @permission_classes([])
 def location(request):
     response_str = ""
-    print(request.data)
-    #role = request.data['action']['parameters']['name']['value']
-    role = "엄마"
+    role = request.data['action']['parameters']['name']['value']
     if role == "첫째" or role == "둘째":
         response_str = role + "는 서울시 관악구 봉천동 4-1 에 있어!"
     else:
         response_str = role + "는 아직 등록 되어있지 않아."
-    a = f'"str":"{response_str}"'
-    print(NUGU_JSON_FIRST + a + NUGU_JSON_SECOND)
-    return JsonResponse(NUGU_JSON_FIRST + a + NUGU_JSON_SECOND,
-                        status=status.HTTP_200_OK)
+    NUGU_OBJ['response']['output']['str'] = response_str
+    return JsonResponse(NUGU_OBJ, status=status.HTTP_200_OK)
