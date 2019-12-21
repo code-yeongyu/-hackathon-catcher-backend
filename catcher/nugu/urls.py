5e6e7619d114d75286b51b2118db3c4558e62008
YeonGyu <public.kim.yeon.gyu@gmail.com>
2019-12-22 05:16:40 +0900

    fix

--- catcher/nugu/urls.py
+++ catcher/nugu/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from nugu import views
 
 urlpatterns = [
-    url(r'^location$', views.location),
-    url(r'^location/$', views.location)
+    url(r'^Location_action$', views.location),
 ]
